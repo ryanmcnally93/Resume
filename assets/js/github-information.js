0d8f98d89f5c1016cb14e547a38d6e220831c2b5
Ryan McNally <ryanmcnallymusic@outlook.com>
2023-03-26 14:57:18 +0000

    Fetch repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -11,8 +11,29 @@ function userInformationHTML(user) {
                     <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.pulic_repos}</p>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
+}
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`
+    }
+
+    var listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
 }
 
 function fetchGitHubInformation(event) {
@@ -29,11 +50,14 @@ $("#gh-user-data").html(
     </div>`);
 
 $.when(
-    $.getJSON(`https://api.github.com/users/${username}`)
-).then(
-    function(response) {
-        var userData = response;
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+    function(firstResponse, secondResponse) {
+        var userData = firstResponse[0];
+        var repoData = secondResponse[0];
         $("#gh-user-data").html(userInformationHTML(userData));
+        $("#gh-repo-data").html(repoInformationHTML(repoData));
     },
     function(errorResponse) {
         if (errorResponse.status === 404) {
