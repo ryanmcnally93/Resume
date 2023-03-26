0012973d744f945a3c2148eb3dea4e545c54c3d5
Ryan McNally <ryanmcnallymusic@outlook.com>
2023-03-26 15:53:19 +0000

    Add comments for understanding

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,7 @@
 function userInformationHTML(user) {
+    // THIS IS THE INFORMATION FORMAT WHEN CORRECT INPUT
+    // (USER) IS A URL IN THE API
+    // NOTE THAT THE USERS AVATAR AND FOLLOWERS ARE ALSO LOCATED
     return `
         <h2>${user.name}
             <span class="small-name">
@@ -16,16 +19,19 @@ function userInformationHTML(user) {
 }
 
 function repoInformationHTML(repos) {
+    // MESSAGE FOR NO REPOS
     if (repos.length == 0) {
         return `<div class="clearfix repo-list">No repos!</div>`
     }
 
     var listItemsHTML = repos.map(function(repo) {
+        // REPONSE FOR ANY REPOSITORY, ADDED AS LIST ITEMS
         return `<li>
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </li>`
     });
 
+    // FORMAT FOR WHICH ANSWER IS SHOWN
     return `<div class="clearfix repo-list">
                 <p>
                     <strong>Repo List:</strong>
@@ -37,35 +43,47 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    // MAKES DISPLAY EMPTY WHEN NO INPUT
     $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
+    // THIS MESSAGE DISPLAYS IF FIELD IS EMPTY
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
+    //DISPLAYS LOADING GIF
+    $("#gh-user-data").html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
 
-$("#gh-user-data").html(
-    `<div id="loader">
-        <img src="assets/css/loader.gif" alt="loading..." />
-    </div>`);
-
-$.when(
+    // PROMISE STATEMENT TO SEARCH GITHUB API
+    $.when(
+        // API URL'S
         $.getJSON(`https://api.github.com/users/${username}`),
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
     function(firstResponse, secondResponse) {
+        // PROMISES SEND BACK ARRAYS WHEN MULTIPLE, SELECT [0]
         var userData = firstResponse[0];
         var repoData = secondResponse[0];
         $("#gh-user-data").html(userInformationHTML(userData));
         $("#gh-repo-data").html(repoInformationHTML(repoData));
     },
+    // ERROR MESSAGE FOR WHEN THE FUNCTION DOES NOT WORK
     function(errorResponse) {
+        // 404 IS PAGE NOT FOUND
         if (errorResponse.status === 404) {
             $("#gh-user-data").html(
                 `<h2>No info found for user ${username}</h2>`);
+        } else if (errorResponse.status === 403) {
+            // LETS USER KNOW API RESTRICT TIME PERIOD
+            var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
+            $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}`);
         } else {
+            // NOT SURE WHAT THE ERROR IS, SO JUST DISPLAYING JAVASCRIPT ERROR MESSAGE
             console.log(errorResponse);
             $("#gh-user-data").html(
                 `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
@@ -73,4 +91,5 @@ $.when(
     });
 }
 
+// DISPLAYS OCTOCATS INFORMATION BY TRIGGERING THE INPUT'S FUNCTION
 $(document).ready(fetchGitHubInformation);
