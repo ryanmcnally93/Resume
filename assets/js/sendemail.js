71e138a5b93e1871b514e93adfc02f2ce0c0afe0
Ryan McNally <ryanmcnallymusic@outlook.com>
2023-03-27 15:28:04 +0000

    Integrate email function and google maps and github API's

---
+++ assets/js/sendemail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_aev1ha8", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "project_request":contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;
+}
